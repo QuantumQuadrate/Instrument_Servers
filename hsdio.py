0b01e94eb650b11273ef4224d31584542655d9fa
prhuft <31295508+prhuft@users.noreply.github.com>
2020-04-02 17:22:47 -0500

    HSDIO.load_xml finished

--- hsdio.py
+++ hsdio.py
@@ -4,15 +4,16 @@
 # I'll start to fill in the gaps to make this functioning code - Juan
 
 from ctypes import * # open to suggestions on making this better with minimal obstruction to workflow
+import numpy as np
 import xml.etree.ElementTree as ET
 import os
 import struct
 
 ## local class imports
-from trigger import Trigger
+from trigger import Trigger, StartTrigger
 from waveform import Waveform
 
-class HSDIO(Instrument): # could inherit from an Instrument class if helpful
+class HSDIO: # could inherit from an Instrument class if helpful
 
 	dllpath32 = os.path.join("C:\Program Files (x86)\IVI Foundation\IVI\Bin", "niHSDIO.dll")
 	dllpath64 = os.path.join("C:\Program Files\IVI Foundation\IVI\Bin", "niHSDIO_64.dll")
@@ -36,19 +37,16 @@ class HSDIO(Instrument): # could inherit from an Instrument class if helpful
 		
 		## device settings
 		self.enablePulses = False
-		self.resourceNames = []
+		self.resourceNames = np.array([], dtype=str)
 		self.clockRate = 2*10**7 # 20 MHz
 		self.hardwareAlignmentQuantum = 1 # (in samples)
-		self.activeChannels = []
-		self.initialStates = []
-		self.idleStates = []
+		self.activeChannels = np.array([], dtype=c_int32)
+		self.initialStates = np.array([], dtype=str)
+		self.idleStates = np.array([], dtype=str)
 		self.pulseGenScript = """script script 1 
 								      wait 1
 								   end script"""
-		
 		self.scriptTriggers = []
-		
-		# instrument handles. not really sure what these are yet
 		self.instrumentHandles = []
 		
 		# whether or not we've actually populated the attributes above
@@ -70,29 +68,29 @@ class HSDIO(Instrument): # could inherit from an Instrument class if helpful
 				
 				# handle each tag by name:
 				if child.tag == "enable":
-					print_txt(child) # DEBUGGING
+					self.print_txt(child) # DEBUGGING
 					self.enablePulses = bool(child.text)
 				
 				elif child.tag == "description":
-					print_txt(child) # DEBUGGING
+					self.print_txt(child) # DEBUGGING
 					self.description = child.text
 				
 				elif child.tag == "resourceName":
-					print_txt(child) # DEBUGGING
-					resources = child.text.split(",")
+					self.print_txt(child) # DEBUGGING
+					resources = np.array(child.text.split(","))
 					self.resourceNames = resources
 					
 				elif child.tag == "clockRate":
 					clockRate = float(child.text)
-					print_txt(child) # DEBUGGING
+					self.print_txt(child) # DEBUGGING
 					self.clockRate = clockRate
 					
 				elif child.tag == "hardwareAlignmentQuantum":
-					print_txt(child) # DEBUGGING
+					self.print_txt(child) # DEBUGGING
 					self.hardwareAlignmentQuantum = child.text
 				
 				elif child.tag == "triggers":
-					print_txt(child) # DEBUGGING
+					self.print_txt(child) # DEBUGGING
 					
 					if type(child) == ET.Element:
 						
@@ -109,7 +107,7 @@ class HSDIO(Instrument): # could inherit from an Instrument class if helpful
 					  
 				elif child.tag == "waveforms":
 
-					#print_txt(child) # HUGE WAVEFORM STRING PLZ BE CAREFUL
+					#self.print_txt(child) # HUGE WAVEFORM STRING PLZ BE CAREFUL
 					print("found a waveform") #TODO: change to logger
 	
 					# TODO: wrap in load waveform xml
@@ -125,7 +123,35 @@ class HSDIO(Instrument): # could inherit from an Instrument class if helpful
 								wvform = Waveform()
 								wvform.init_from_xml(wvf_child)
 								self.waveformArr.append(wvform)
+								
+				elif child.tag == "script":
+					self.print_txt(child) # DEBUGGING
+					self.pulseGenScript
+					
+				elif child.tag == "startTrigger":
+					self.startTrigger = StartTrigger()
+					self.startTrigger.init_from_xml(child)
+									
+				elif child.tag == "InitialState":
+					self.print_txt(child) # DEBUGGING
+					self.initialStates = np.array(child.text.split(","))
 				
+				elif child.tag == "IdleState":
+					self.print_txt(child) # DEBUGGING
+					self.idleStates = np.array(child.text.split(",")) 
+				
+				elif child.tag == "ActiveChannels":
+					self.print_txt(child) # DEBUGGING
+					self.activeChannels = np.array(child.text.split("\n")) 
+				
+				else:
+					# TODO: replace with logging
+					print("Not a valid XML tag for HSDIO initialization")
+					
+		# TODO: replace with logging
+		print("HSDIO XML Loaded")
+		
+					
 	def init(self):
 		"""
 		set up the triggering, initial states, script triggers, etc
@@ -147,6 +173,7 @@ class HSDIO(Instrument): # could inherit from an Instrument class if helpful
 		pulse_gen = self.pulseGenScript
 					  
 		for wf in waveform_arr:
+			pass
 			# write each waveform wf to the PC memory: (or not actually according to Juan)
 			
 			#call the c function: niHSDIO_WriteNamedWaveformWDT 
@@ -156,11 +183,15 @@ class HSDIO(Instrument): # could inherit from an Instrument class if helpful
 			#http://zone.ni.com/reference/en-XX/help/370520P-01/hsdio/writing_waveforms_to_your_instrument/
 			
 	def settings(self, wf_arr, wf_names):
+		pass
 		# the labview code has HSDIO.settings specifically for reading out the 
 		# settings on the front panel. for debugging, we could just have this
 		# log certain HSDIO attributes 
 		
 		# log stuff, call settings in the server code for debugging?
+		
+	def print_txt(self, node): # for debugging
+		print(f"{node.tag} = {node.text}") # TODO replace with logging
 		
 	def chk(self,er_code):
 		"""
--- trigger.py
+++ trigger.py
@@ -1,4 +1,5 @@
 from ctypes import c_uint32
+import xml.etree.ElementTree as ET
 
 class Trigger:
 	""" Trigger data type for PXI server """
@@ -10,12 +11,12 @@ class Trigger:
 	levels = {"High Level": c_uint32(0),
 			  "Low Level": c_uint32(1)}
 	
-	def __init__(self, trig_id="", source="", trig_type=types["Edge"], 
+	def __init__(self, trigID="", source="", trigType=types["Edge"], 
 				 edge=edges["Rising Edge"], level=levels["High Level"]):
 		
-		self.id = trig_id
+		self.trigID = trigID
 		self.source = source # PFI 0
-		self.type = trig_type
+		self.trigType = trigType
 		self.edge = edge
 		self.level = level
 		
@@ -24,38 +25,99 @@ class Trigger:
 		re-initialize attributes for existing Trigger from children of node. 
 		'node' is of type xml.etree.ElementTree.Element, with tag="trigger"
 		"""
-		
-		if child.tag == "id": 
-			self.id = child.text # script trigger 0
-
-		elif child.tag == "source":
-			self.source = child.text # PFI 0
-
-		elif child.tag == "type":
-
-			if child.text == "level":
-				self.type = Trigger.types["Level"]
-
-			# else, stick with default initialization
-
-		elif child.tag == "edge":
-
-			if child.text == "Falling Edge":
-				self.edge = Trigger.edges["Falling Edge"]
-				pass
-
-			# else, stick with default initialization
-
-		elif child.tag == "level":
-
-			if child.text == "Low Level":
-				self.level = Trigger.levels["Low Level"]
-				pass
+		for child in node:
 			
-		else:
-			# TODO: replace with logger
-			print("Not a valid trigger attribute") 
+			if type(child) == ET.Element:
+			
+				if child.tag == "id": 
+					self.trigID = child.text # script trigger 0
+
+				elif child.tag == "source":
+					self.source = child.text # PFI 0
+
+				elif child.tag == "type":
+
+					if child.text == "level":
+						self.trigType = self.trigTypes["Level"]
+
+					# else, stick with default initialization
+
+				elif child.tag == "edge":
+
+					if child.text == "Falling Edge":
+						self.edge = self.edges["Falling Edge"]
+						pass
+
+					# else, stick with default initialization
+
+				elif child.tag == "level":
+
+					if child.text == "Low Level":
+						self.level = self.levels["Low Level"]
+						pass
+					
+				else:
+					# TODO: replace with logger
+					print("Not a valid trigger attribute") 
 		
 	def __repr__(self): # mostly for debugging
-		return (f"Trigger(id={self.id}, source={self.source}, "
-				f"type={self.type}, edge={self.edge}, level={self.level})")
+		return (f"Trigger(id={self.trigID}, source={self.source}, "
+				f"type={self.trigType}, edge={self.edge}, level={self.level})")
+				
+
+# with some refactoring, this could inherit from Trigger
+class StartTrigger: 
+
+	edges = {"Rising Edge": c_uint32(0),
+			 "Falling Edge": c_uint32(1)}
+
+	def __init__(self, waitForStartTrigger=False, source="", description="", 
+				 edge=edges["Rising Edge"]):
+		self.waitForStartTrigger = waitForStartTrigger
+		self.source = source
+		self.description = description
+		self.edge = edge
+	
+	def init_from_xml(self, node):
+		"""
+		re-initialize attributes for existing StartTrigger from children of node. 
+		'node' is of type xml.etree.ElementTree.Element, with tag="startTrigger"
+		"""
+		
+		for child in node:
+			
+			if type(child) == ET.Element: 
+		
+				if child.tag == "waitForStartTrigger":
+
+					if child.text.lower() == "true":
+						wait_bool = True
+					else: # if it wasn't True, assume False
+						wait_bool = False
+					self.waitForStartTrigger = wait_bool
+					
+				elif child.tag == "source":
+					self.source = child.text # PFI 0
+					
+				elif child.tag == "description":
+					self.description = child.text
+					
+				elif child.tag == "edge":
+					
+					if child.text == "falling":
+						self.edge = StartTrigger.edges["Falling"]
+					elif child.text == "rising":
+						# this is the default value, do nothing
+						pass
+					else:
+						# TODO: replace with logger
+						print("Invalid edge type for StartTrigger")
+						
+				else:
+					print("Unrecognized XML tag for StartTrigger")
+		
+	
+	def __repr__(self): # mostly for debugging
+		return (f"StartTrigger(waitForStartTrigger={self.waitForStartTrigger}, "
+				f"source={self.source}, description={self.description}, "
+				f"edge={self.edge})")
