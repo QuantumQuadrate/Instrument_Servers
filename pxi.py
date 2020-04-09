c52b699da56b9b4a7b64cbec9ef20d87be243372
c-poole <codemeister4@gmail.com>
2020-04-08 20:36:54 -0500

    adjust structure to be more in line with labview

---
+++ pxi.py
@@ -0,0 +1,95 @@
+import socket
+from typing import Tuple
+import logging
+import threading
+
+
+class PXI:
+
+    def __init__(self, address: Tuple[str, int]):
+        self.logger = logging.getLogger(str(self.__class__))
+        self.listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listening_socket.bind(address)
+        self.listening_socket.listen()
+
+        # TODO: Make a method to change stop_connections and reset_connections
+        # TODO: These are the backend variables normally adjusted by clicking on the GUI
+        # TODO: In the LabVIEW program
+        self.stop_connections = False
+        self.reset_connection = False
+        self.current_connection = None
+        self.last_received_xml = ""
+
+        self.network_thread = None
+
+    def launch_experiment_thread(self):
+        """
+        TODO: make target method for this launcher
+        """
+        pass
+
+    def launch_network_thread(self):
+        self.network_thread = threading.Thread(
+            target=self.network_loop,
+            name='Network Thread'
+        )
+        self.network_thread.setDaemon(False)
+        self.network_thread.start()
+
+    def network_loop(self):
+        """
+        Check for incoming connections and messages on those connections
+        """
+        while not self.stop_connections:
+            self.reset_connection = False
+            self.current_connection, client_address = self.listening_socket.accept()
+            self.logger.info(f"Started connection with {client_address}")
+            while not (self.reset_connection or self.stop_connections):
+                try:
+                    self.receive_message()
+                except socket.timeout:
+                    pass
+            self.logger.info(f"Closing connection with {client_address}")
+            self.current_connection.close()
+            self.current_connection.shutdown()
+
+    def receive_message(self):
+        """
+        listens for a message from cspy over the network.
+
+        messages from cspy are encoded in the following way:
+            message = 'MESG' + str(len(body)) + body
+
+        """
+        # Read first 4 bytes looking for a specific message header
+        self.current_connection.settimeout(0.3)
+        header = self.current_connection.recv(4)
+        self.logger.info(f"header was read as {header}")
+        if header == b'MESG':
+            self.logger.info("We got a message! now to handle it.")
+            # Assume next 4 bytes contains the length of the remaining message
+            length_bytes = self.current_connection.recv(4)
+            length = int.from_bytes(length_bytes, byteorder='big')
+            self.logger.info(f"I think the message is {length} bytes long.")
+            self.current_connection.settimeout(20)
+            message = self.current_connection.recv(length)
+            if len(message) == length:
+                self.logger.info("message received with expected length.")
+                self.last_received_xml = message
+            else:
+                self.logger.info(f"Something went wrong,"
+                                 f" I only found {len(message)} bytes to read!")
+        else:
+            self.logger.info("We appear to have received junk. Clearing buffer.")
+            self.current_connection.settimeout(0.01)
+            try:
+                while not (self.reset_connection or self.stop_connections):
+                    if self.current_connection.recv(4096) == "":
+                        break
+            except socket.timeout:
+                pass
+            finally:
+                self.reset_connection = True
+
+
+
--- test_server.py
+++ test_server.py
@@ -1,6 +1,6 @@
-import socket
 import logging
 import colorlog
+from pxi import PXI
 
 
 def setup_logging_handlers():
@@ -42,56 +42,21 @@ def setup_logging_handlers():
 
 
 if __name__ == '__main__':
-
+    print("zero")
     setup_logging_handlers()
     logger = logging.getLogger(__name__)
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+    print("one")
     hostname = 'localhost'
-    # 9000 for DDS, 9001 for PXI
     port = 9001
-
     address = (hostname, port)
 
     logger.info(f'starting up on {hostname} port {port}')
-    sock.bind(address)
-    maximum_simultaneous_connections = 1
-    sock.listen(maximum_simultaneous_connections)
+    print("two")
+    experiment = PXI(address)
+    experiment.launch_network_thread()
+    experiment.launch_experiment_thread()
+    print("three")
 
-    while True:
-        connection, client_address = sock.accept()
-        logger.info(f"Receiving connection from {client_address}")
-        try:
-            # Read first 4 bytes looking for a specific message header
-            connection.settimeout(0.3)
-            header = connection.recv(4)
-            logger.info(f"header was read as {header}")
-            if header == b'MESG':
-                logger.info("We got a message! now to handle it.")
-                # Assume next 4 bytes contains the length of the remaining message
-                length_bytes = connection.recv(4)
-                length = int.from_bytes(length_bytes, byteorder='big')
-                logger.info(f"I think the message is {length} bytes long.")
-                connection.settimeout(20)
-                message = connection.recv(length)
-                if len(message) == length:
-                    logger.info("message received with expected length. Sending it back.")
-                    # echo the message back since we haven't implemented actual handling yet
-                    connection.sendall(header + length_bytes + message)
-                else:
-                    logger.info(f"Something went wrong, I only found {len(message)} bytes to read!")
-            else:
-                logger.info("We appear to have received junk. Clearing buffer.")
-                connection.settimeout(0.01)
-                more_to_clear = True
-                while more_to_clear:
-                    more_to_clear = not connection.recv(4096) == ''
-        except TimeoutError as e:
-            logger.exception(e)
-        except Exception as e:
-            logger.exception(e)
-        finally:
-            logger.info("Closing the connection so I can receive another one.")
-            connection.close()
+
+
 
