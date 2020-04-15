511724daef5a8289953d03b22edba95bccfb4c66
JuanBohorquez3 <jbohorquez@wisc.edu>
2020-04-15 14:02:24 -0500

    Added imaq wrapper check()

--- ni_imaq.py
+++ ni_imaq.py
@@ -16,25 +16,46 @@ import platform
 class NiImaqSession:
     def __init__(self):
         self.imaq = CDLL(os.path.join("C:\Windows\System32", "imaq.dll"))
-        self.interface_id = c_ulong(0)
-        self.session_id = c_ulong(0)
+        self.interface_id = c_uint32(0)
+        self.session_id = c_uint32(0)
 
     def check(
             self,
             error_code: int,
             traceback_msg: str = None):
         """
-
-        Args:
-            error_code : error code which encodes status of operation.
-
-            traceback_msg ():
-
+        Checks error_code with self.imaq to get out a descriptive error message and prints(logs)
+        error/warning  if operation was unsuccessful
         TODO : Proper logging
         TODO : Proper traceback
         TODO : Raise Errors and Warnings where appropriate
 
-        Returns:
-
+        Args:
+            error_code : error code which encodes status of operation.
+                0 = Success, positive values = Warnings , negative values = Errors
+            traceback_msg: message useful for traceback
         """
+        if error_code == 0:
+            return
 
+        err_msg = c_char_p("".encode('utf-8'))
+
+        self.imaq.imgShowError(
+            error_code,  # IMG_ERR
+            err_msg)     # char*
+
+        if error_code < 0:
+            code_type = "Error Code"
+        elif error_code > 0:
+            code_type = "Warning Code"
+        else:
+            code_type = ""
+        if traceback_msg is None:
+            message = f"{code_type} {error_code} :\n {err_msg}"
+        else:
+            message = f"{code_type} {error_code} in {traceback_msg}:\n {err_msg}"
+
+        print(message)
+        return
+
+    def open_interface(self,dev_addr):
