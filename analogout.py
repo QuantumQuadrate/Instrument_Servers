792d8b76f0e2fb5265586344303fcf4a671a467f
prhuft <31295508+prhuft@users.noreply.github.com>
2020-04-22 19:51:51 -0500

    ao.init, xml parsing

--- analogout.py
+++ analogout.py
@@ -7,6 +7,8 @@ SaffmanLab, University of Wisconsin - Madison
 import nidaqmx
 import numpy as np
 import xml.etree.ElementTree as ET
+import csv
+from io import StringIO
 
 
 class AnalogOutput:
@@ -14,7 +16,67 @@ class AnalogOutput:
     def __init__(self):
 
         self.enable = False
-        self.physical_channels = ""
+        #self.physicalChannels = ""
+        #self.minValue = 
+        #self.maxValue
+
+        self.startTrigger = StartTrigger()
+
+
+    # TODO: good candidate for a unit test
+    def wave_from_str(self, wave_str, delim=' '):
+        """
+        Efficiently build waveform from a string
+
+        Args:
+            'wave_str': (str) a (possibly multi-line) string of space-delimited
+                float-convertable values. 
+            'delim': (str, optional) the value delimiter. ' ' by default
+        Returns:
+            'wave_arr': (np.ndarray, float) the waveform with one row per line 
+                in wave_str, and one column per value in a line
+        """
+
+        with StringIO(wstr) as f:
+            reader = csv.reader(f, delimiter=delim)
+            cols = len(next(reader))
+            #print(f"cols={cols}") # DEBUG
+            try:
+                rows = sum([1 for row in reader]) + 1
+            except StopIteration:
+                rows = 1
+                #print(f"rows={rows}") # DEBUG
+            wave_arr = np.empty((rows, cols), float)
+    
+        with StringIO(wstr) as f:
+            reader = csv.reader(f, delimiter=delim)
+            for i,row in enumerate(reader):
+                #print(row, len(row),'\n') # DEBUG
+                wave_arr[i,:] = row
+        #print(wave) # DEBUG
+
+        return wave_arr
+
+
+    def str_to_bool(boolstr):
+        """ 
+        return True or False case-insensitively for a string 'true' or 'false'
+
+        Args: 
+            'boolstr': string to be converted; not case-sensitive
+        Return:
+            'boolean': True or False. 
+        """
+
+        boolstr = boolstr.lower()
+        if boolstr == "true":
+            return True
+        elif boolstr == "false":
+            return False
+        else:
+            print("Expected a string 'true' or 'false' but received {boolstr}")
+            raise
+
 
     def load_xml(self, node):
         """
@@ -30,43 +92,60 @@ class AnalogOutput:
 
         for child in node: 
 
-            if child.tag == "enable":
-                enable = False
-                if child.text.lower() == "true":
-                    enable = True
-                self.enable = enable
+            # not sure if this is necessary... could probably remove
+            if type(child) == ET.Element:
 
-            elif child.tag == "physicalChannels":
-                self.physical_channels = child.text
+                if child.tag == "enable":
+                    self.enable = str_to_bool(child.text)
 
-            elif child.tag == ""
+                elif child.tag == "physicalChannels":
+                    self.physicalChannels = child.text
 
-            #elif child.tag == 
+                elif child.tag == "minimum":
+                    self.minValue = float(child.text)
 
-            #elif child.tag == 
+                elif child.tag == "maximum":
+                    self.maxValue = float(child.text)
 
-            #elif child.tag == 
+                elif child.tag == "clockRate":
+                    self.sampRate = float(child.text) # samples per second in LabVIEW
 
-            #elif child.tag == 
+                elif child.tag == "waveform":
+                    self.waveforms = wave_from_str(child.text)
 
-            #elif child.tag == 
+                elif child.tag == "waitForStartTrigger":
+                    self.waitForStartTrig = str_to_bool(child.text)
 
-            #elif child.tag == 
+                elif child.tag == "exportStartTrigger":
+                    self.exportStartTrig = str_to_bool(child.text)
 
-            #elif child.tag == 
+                elif child.tag == "triggerSource":
+                    self.startTrigSource = child.text
 
-            #elif child.tag == 
+                elif child.tag == "exportStartTriggerDestination":
+                    self.exportTrigger.outputTerminal = child.text # TODO implement the exportTrigger
 
-            #elif child.tag == 
+                elif child.tag == "triggerEdge":
+                    try:
+                        self.startTrigger.edge = StartTrigger.ao_edges[child.text]
+                    except KeyError as e:
+                        # TODO: replace with logger
+                        print(f"Not a valid {child.tag} value {child.text} \n {e}")
+                        raise
 
-            #elif child.tag == 
+                # TODO: external clock could be a class as in LabVIEW. TBD.
+                elif child.tag == "useExternalClock":
+                    self.useExternalClock = str_to_bool(child.text)
 
-            #elif child.tag == 
-                
-            #else:
-            #    # TODO: replace with logger
-            #    print(f"Unrecognized XML tag {child.tag} in <AnalogOutput>)
+                elif child.tag == "externalClockSource":
+                    self.externalClockSource = child.text
 
+                elif child.tag == "maxExternalClockRate":
+                    self.externalClockRateMax = float(child.text)
+
+                else:
+                    # TODO: replace with logger
+                    print(f"Unrecognized XML tag {child.tag} in <AnalogOutput>")
 
 
     def init(self):
--- trigger.py
+++ trigger.py
@@ -9,122 +9,126 @@ Author(s): Preston Huft
 """
 
 class Trigger:
-	""" Trigger data type for PXI server """
-	# TODO: make these into ordered dicts or have someway
-	types = {"Edge": 0,
-			 "Level": 1}
-	edges = {"Rising Edge": 12,
-			 "Falling Edge": 13}
-	levels = {"High Level": 34,
-			  "Low Level": 35}
-	
-	def __init__(self, trigID="", source="", trigType=types["Edge"], 
-				 edge=edges["Rising Edge"], level=levels["High Level"]):
-		
-		self.trigID = trigID
-		self.source = source # PFI 0
-		self.trigType = trigType
-		self.edge = edge
-		self.level = level
-		
-	def init_from_xml(self, node):
-		"""
-		re-initialize attributes for existing Trigger from children of node. 
-		'node' is of type xml.etree.ElementTree.Element, with tag="trigger"
-		"""
-		for child in node:
-			
-			if type(child) == ET.Element:
-			
-				if child.tag == "id": 
-					self.trigID = child.text # script trigger 0
+    """ Trigger data type for PXI server """
+    # TODO: Give these default keys like in the Hamamatsu class
+    types = {"Edge": 0,
+             "Level": 1}
+    edges = {"Rising Edge": 12,
+             "Falling Edge": 13}
+    levels = {"High Level": 34,
+              "Low Level": 35}
+    
+    def __init__(self, trigID="", source="", trigType=types["Edge"], 
+                 edge=edges["Rising Edge"], level=levels["High Level"]):
+        
+        self.trigID = trigID
+        self.source = source # PFI 0
+        self.trigType = trigType
+        self.edge = edge
+        self.level = level
+        
+    def init_from_xml(self, node):
+        """
+        re-initialize attributes for existing Trigger from children of node. 
+        'node' is of type xml.etree.ElementTree.Element, with tag="trigger"
+        """
+        for child in node:
+            
+            if type(child) == ET.Element:
+            
+                if child.tag == "id": 
+                    self.trigID = child.text # script trigger 0
 
-				elif child.tag == "source":
-					self.source = child.text # PFI 0
+                elif child.tag == "source":
+                    self.source = child.text # PFI 0
 
-				elif child.tag == "type":
+                elif child.tag == "type":
 
-					if child.text == "level":
-						self.trigType = self.trigTypes["Level"]
+                    if child.text == "level":
+                        self.trigType = self.trigTypes["Level"]
 
-					# else, stick with default initialization
+                    # else, stick with default initialization
 
-				elif child.tag == "edge":
+                elif child.tag == "edge":
 
-					if child.text == "Falling Edge":
-						self.edge = self.edges["Falling Edge"]
-						pass
+                    if child.text == "Falling Edge":
+                        self.edge = self.edges["Falling Edge"]
+                        pass
 
-					# else, stick with default initialization
+                    # else, stick with default initialization
 
-				elif child.tag == "level":
+                elif child.tag == "level":
 
-					if child.text == "Low Level":
-						self.level = self.levels["Low Level"]
-						pass
-					
-				else:
-					# TODO: replace with logger
-					print("Not a valid trigger attribute") 
-		
-	def __repr__(self): # mostly for debugging
-		return (f"Trigger(id={self.trigID}, source={self.source}, "
-				f"type={self.trigType}, edge={self.edge}, level={self.level})")
-				
+                    if child.text == "Low Level":
+                        self.level = self.levels["Low Level"]
+                        pass
+                    
+                else:
+                    # TODO: replace with logger
+                    print("Not a valid trigger attribute") 
+        
+    def __repr__(self): # mostly for debugging
+        return (f"Trigger(id={self.trigID}, source={self.source}, "
+                f"type={self.trigType}, edge={self.edge}, level={self.level})")
+                
 
 # with some refactoring, this could inherit from Trigger
 class StartTrigger: 
 
-	edges = {"Rising Edge": 12,
-			 "Falling Edge": 13}
+    # TODO add Default key
+    edges = {"Rising Edge": 12,
+             "Falling Edge": 13}
 
-	def __init__(self, waitForStartTrigger=False, source="", description="", 
-				 edge=edges["Rising Edge"]):
-		self.waitForStartTrigger = waitForStartTrigger
-		self.source = source
-		self.description = description
-		self.edge = edge
-	
-	def init_from_xml(self, node):
-		"""
-		re-initialize attributes for existing StartTrigger from children of node. 
-		'node' is of type xml.etree.ElementTree.Element, with tag="startTrigger"
-		"""
-		
-		for child in node:
-			
-			if type(child) == ET.Element: 
-		
-				if child.tag == "waitForStartTrigger":
+    ao_edges = {"Rising" : 10280,
+                "Falling": 10171,
+                "Default": "Rising"}
 
-					if child.text.lower() == "true":
-						wait_bool = True
-					else: # if it wasn't True, assume False
-						wait_bool = False
-					self.waitForStartTrigger = wait_bool
-					
-				elif child.tag == "source":
-					self.source = child.text # PFI 0
-					
-				elif child.tag == "description":
-					self.description = child.text
-					
-				elif child.tag == "edge":
-					
-					if child.text == "falling":
-						self.edge = StartTrigger.edges["Falling"]
-					elif child.text == "rising":
-						# this is the default value, do nothing
-						pass
-					else:
-						# TODO: replace with logger
-						print("Invalid edge type for StartTrigger")
-						
-				else:
-					print("Unrecognized XML tag for StartTrigger")
-		
-	
-	def __repr__(self): # mostly for debugging
-		return (f"StartTrigger(waitForStartTrigger={self.waitForStartTrigger}, "
-				f"source={self.source}, description={self.description}, "
-				f"edge={self.edge})")
+    def __init__(self, waitForStartTrigger=False, source="", description="", 
+                    edge=edges["Rising Edge"]):
+        self.waitForStartTrigger = waitForStartTrigger
+        self.source = source
+        self.description = description
+        self.edge = edge
+
+    def init_from_xml(self, node):
+        """
+        re-initialize attributes for existing StartTrigger from children of node. 
+        'node' is of type xml.etree.ElementTree.Element, with tag="startTrigger"
+        """
+        for child in node:
+
+            if type(child) == ET.Element: 
+
+                if child.tag == "waitForStartTrigger":
+
+                    if child.text.lower() == "true":
+                        wait_bool = True
+                    else: # if it wasn't True, assume False
+                        wait_bool = False
+                    self.waitForStartTrigger = wait_bool
+
+                elif child.tag == "source":
+                    self.source = child.text # PFI 0
+
+                elif child.tag == "description":
+                    self.description = child.text
+
+                elif child.tag == "edge":
+                
+                    if child.text == "falling":
+                        self.edge = StartTrigger.edges["Falling"]
+                    elif child.text == "rising":
+                        # this is the default value, do nothing
+                        pass
+                    else:
+                        # TODO: replace with logger
+                        print("Invalid edge type for StartTrigger")
+
+            else:
+                print("Unrecognized XML tag for StartTrigger")
+
+
+        def __repr__(self): # mostly for debugging
+            return (f"StartTrigger(waitForStartTrigger={self.waitForStartTrigger}, "
+                    f"source={self.source}, description={self.description}, "
+                    f"edge={self.edge})")
